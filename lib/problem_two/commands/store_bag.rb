2b0fc3ec09660c034b1c59e09817a6a3aaab5579
Matt Snyder <matt@cranialpulse.com>
2012-07-26 16:31:43 -0400

    The StoreBag command now stores the bag and creates a ticket

--- lib/problem_two/commands/store_bag.rb
+++ lib/problem_two/commands/store_bag.rb
@@ -1,7 +1,16 @@
+require './lib/problem_two/locker_room'
+require './lib/problem_two/ticket_service'
+
 module ProblemTwo
   module Commands
     class StoreBag
       def initialize(size)
+        @size = size
+      end
+
+      def execute
+        location = LockerRoom.store @size
+        TicketService.create location
       end
     end
   end
--- spec/problem_two/commands/store_bag_spec.rb
+++ spec/problem_two/commands/store_bag_spec.rb
@@ -1,3 +1,12 @@
 require 'rspec/given'
 
 require './lib/problem_two/commands/store_bag'
+
+
+describe "storing a bag" do
+  Given (:ticket) { mock "ticket for my locker" }
+  Given { ProblemTwo::LockerRoom.should_receive(:store).with("asize").and_return("alocation")  }
+  Given { ProblemTwo::TicketService.should_receive(:create).with("alocation").and_return ticket }
+  When (:result) { ProblemTwo::Commands::StoreBag.new("asize").execute }
+  Then { result.should == ticket }
+end
